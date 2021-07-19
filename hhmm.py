9b7182cf47e34ac6d382cb08c8a62801b3e49176
sss1 <sss1@andrew.cmu.edu>
2021-07-19 14:43:46 +0200

    Basic version of HMM that fits a single trial.

--- hhmm.py
+++ hhmm.py
@@ -2,28 +2,30 @@ import numpy as np
 from matplotlib import pylab as plt
 import scipy.stats
 
-import tensorflow.compat.v2 as tf
-tf.enable_v2_behavior()
+import tensorflow as tf
 import tensorflow_probability as tfp
 from tensorflow_probability import distributions as tfd
 
 import load_subjects as ls
 
-# GENERATE TRAINING DATA
-true_means = np.array([[40, 30], [15, 5], [25, 15], [15, 40]], dtype=np.float32)
-true_durations = [10, 20, 10, 35]
-true_Sigma = [[9.0, 0.0], [0.0, 1.0]]
 
-observations = np.concatenate([
-  scipy.stats.multivariate_normal(mean=mean, cov=true_Sigma).rvs(num_steps)
-    for (mean, num_steps) in zip(true_means, true_durations)
-]).astype(np.float32)
-
-plt.plot(observations[:,0], observations[:,1], )
+trial_len = 600  # For now, we only consider the first 10s of each trial
+trials_to_include = range(1, 11)  # Omit practice trials
+num_trials = len(trials_to_include)
+num_iters = 1001  # Number of training iterations
+num_states = 7
 
 # LOAD DATA
-# TODO(sss1): Restructure data into a tensor
-# subjects = ls.load_all_data()
+subjects = ls.load_all_data()
+# observations = np.zeros((num_trials, trial_len, 2), dtype=np.float32)
+# true_means = np.zeros((num_trials, trial_len, num_states, 2), dtype=np.float32)
+# for trial_idx, trial in enumerate(trials_to_include):
+#   observations[trial_idx, :, :] = subjects['0'].experiments['noshrinky'].datatypes['eyetrack'].trials[trial].data[:trial_len, 1:]
+#   print(subjects['0'].experiments['noshrinky'].datatypes['eyetrack'].trials[trial].proportion_missing)
+#   true_means[trial_idx, :, :, :] = subjects['0'].experiments['noshrinky'].datatypes['trackit'].trials[trial].object_positions[:, :trial_len, :].transpose(1, 0, 2)
+trial = 0
+observations = np.float32(subjects['0'].experiments['noshrinky'].datatypes['eyetrack'].trials[trial].data[:trial_len, 1:])
+true_means = np.float32(subjects['0'].experiments['noshrinky'].datatypes['trackit'].trials[trial].object_positions[:, :trial_len, :].transpose(1, 0, 2))
 
 def sigmoid(x):
   """Computes the sigmoid function."""
@@ -34,8 +36,6 @@ def logit(x):
   return - tf.math.log(1. / x - 1.)
 
 # CONSTRUCT HMM WITH TRAINABLE EMISSION DISTRIBUTION
-num_states = 4
-
 # pi is distribution of the initial object of attention.
 # We represent it in terms of logit-probabilities and then
 # take sigmoids+normalize after optimization, as this is much simpler than
@@ -44,7 +44,7 @@ trainable_logit_pi_T = tf.Variable(logit(1/num_states), name='logit_pi_T', dtype
 
 # Pi is the transition matrix of the object of attention.
 # We represent it in terms of logits too.
-switch_prob = 0.05
+switch_prob = 0.005
 self_transition_prob = logit(1. - switch_prob)
 switch_transition_prob = logit(switch_prob / (num_states-1))
 trainable_logit_tau_T2 = tf.Variable(self_transition_prob, name='logit_tau_T2', dtype='float32')
@@ -52,21 +52,23 @@ trainable_logit_tau_DT = tf.Variable(switch_transition_prob, name='logit_tau_DT'
 trainable_logit_tau_D2 = tf.Variable(self_transition_prob, name='logit_tau_D2', dtype='float32')
 
 # Sigma is the (diagonal) covariance matrix of gaze around the attended object.
-trainable_Sigma = tf.Variable([2.0, 2.0], name='Sigma')
+Sigma_0 = 100.0
+trainable_Sigma = tf.Variable([Sigma_0, Sigma_0], name='Sigma')
 
 # We assume that the x- and y-components of the variance (around the attended
 # object) are independent. In reality, this might not be the case, e.g., (if the
 # object is moving diagonally), but we leave it to future work to improve this
 # aspect of the model.
-observation_distribution = tfd.MultivariateNormalDiag(loc=true_means,
-                                                      scale_diag=trainable_Sigma
-                                                     )
+observation_distribution = tfd.MultivariateNormalDiag(
+    loc=true_means, scale_diag=trainable_Sigma)
 
 def construct_pi(logit_pi_T):
   """Constructs initial distribution from logit of pi_T."""
   pi_T = sigmoid(logit_pi_T)
   tau_D0 = (1 - pi_T) / (num_states-1)
-  return [pi_T, tau_D0, tau_D0, tau_D0]
+  pi = [pi_T]
+  pi.extend([tau_D0 for _ in range(num_states-1)])
+  return pi
 
 def construct_Pi(logit_tau_T2, logit_tau_DT, logit_tau_D2):
   """Constructs transition matrix from logits of tau_T2, tau_DT, and tau_D2."""
@@ -75,20 +77,31 @@ def construct_Pi(logit_tau_T2, logit_tau_DT, logit_tau_D2):
   tau_D2 = sigmoid(logit_tau_D2)
   tau_TD = (1 - sigmoid(logit_tau_T2)) / (num_states-1)
   tau_DD = (1 - (sigmoid(logit_tau_DT) + sigmoid(logit_tau_D2))) / (num_states-2)
-  return [[tau_T2, tau_TD, tau_TD, tau_TD],
-          [tau_DT, tau_D2, tau_DD, tau_DD],
-          [tau_DT, tau_DD, tau_D2, tau_DD],
-          [tau_DT, tau_DD, tau_DD, tau_D2]]
+
+  target_row = [tau_T2]
+  target_row.extend([tau_TD for _ in range(num_states-1)])
+  Pi = [target_row]
+  for distractor_row_idx in range(1, num_states):
+    new_row = [tau_DT]
+    for distractor_col_idx in range(1, num_states):
+      if distractor_col_idx == distractor_row_idx:
+        new_row.append(tau_D2)
+      else:
+        new_row.append(tau_DD)
+    Pi.append(new_row)
+
+  return Pi
 
 def construct_hmm(logit_pi_T, logit_tau_T2, logit_tau_DT, logit_tau_D2):
   """Constructs the HMM model from its free parameters."""
-  pi = construct_pi(logit_pi_T)
-  Pi = construct_Pi(logit_tau_T2, logit_tau_DT, logit_tau_D2)
+  pi = tfd.Categorical(probs=construct_pi(logit_pi_T))
+  Pi = tfd.Categorical(probs=construct_Pi(logit_tau_T2, logit_tau_DT, logit_tau_D2))
   return tfd.HiddenMarkovModel(
-      initial_distribution=tfd.Categorical(probs=pi),
-      transition_distribution=tfd.Categorical(probs=Pi),
+      initial_distribution=pi,
+      transition_distribution=Pi,
+      time_varying_observation_distribution=True,
       observation_distribution=observation_distribution,
-      num_steps=len(observations))
+      num_steps=trial_len)
 
 # SPECIFY LOG-LIKELIHOOD TRAINING OBJECTIVE AND OPTIMIZER
 def log_prob():
@@ -97,7 +110,7 @@ def log_prob():
                       trainable_logit_tau_DT,
                       trainable_logit_tau_D2)
   return hmm.log_prob(observations)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
 
 @tf.function(autograph=True)
 def train_op():
@@ -118,9 +131,38 @@ def train_op():
   return neg_log_prob, trainable_logit_pi_T, trainable_logit_tau_T2, \
       trainable_logit_tau_DT, trainable_logit_tau_D2, trainable_Sigma
 
+# RUN FORWARD-BACKWARD ALGORITHM TO COMPUTE MARGINAL POSTERIORS
+def plot_posteriors():
+  hmm = construct_hmm(logit_pi_T,
+                      logit_tau_T2,
+                      logit_tau_DT,
+                      logit_tau_D2)
+  posterior_dists = hmm.posterior_marginals(observations)
+  posterior_probs = posterior_dists.probs_parameter().numpy()
+  
+  def plot_state_posterior(ax, state_posterior_probs, title):
+    ln1 = ax.plot(state_posterior_probs, c='blue', lw=3, label='p(state | observations)')
+    ax.set_ylim(0., 1.1)
+    ax.set_ylabel('posterior probability')
+    labs = [l.get_label() for l in ln1]
+    ax.legend(ln1, labs, loc=4)
+    ax.grid(True, color='white')
+  
+  # PLOT MARGINAL POSTERIORS PROBABILITIES OF EACH STATE
+  fig = plt.figure(figsize=(10, 10))
+  plot_state_posterior(fig.add_subplot(4, 2, 1),
+                       posterior_probs[:, 0],
+                       title="Target")
+  for distractor_idx in range(1, num_states):
+    plot_state_posterior(fig.add_subplot(4, 2, distractor_idx+1),
+                         posterior_probs[:, distractor_idx],
+                         title=f"Distractor {distractor_idx}")
+  plt.tight_layout()
+  plt.show()
+
 # FIT MODEL
 loss_history = []
-for step in range(2001):
+for step in range(num_iters):
   loss, logit_pi_T, logit_tau_T2, logit_tau_DT, logit_tau_D2, Sigma = [t.numpy() for t in train_op()]
 
   # Format probabilities nicely for printing.
@@ -128,50 +170,12 @@ for step in range(2001):
   Pi = np.array([[tau.numpy() for tau in row] for row in construct_Pi(logit_tau_T2, logit_tau_DT, logit_tau_D2)])
 
   loss_history.append(loss)
-  if step % 100 == 0:
+  if step % 500 == 0:
     print("step {}: log prob {} Sigma {}\npi\n{}\nPi\n{}".format(step, -loss, Sigma, pi, Pi))
+    plot_posteriors()
 print("Inferred pi:\n{}".format(pi))
 print("Inferred Pi:\n{}".format(Pi))
 print("Inferred Sigma:\n{}".format(tf.linalg.diag(Sigma)))
-print("True Sigma:\n{}".format(tf.math.sqrt(true_Sigma)))
-
-# RUN FORWARD-BACKWARD ALGORITHM TO COMPUTE MARGINAL POSTERIORS
-hmm = construct_hmm(logit_pi_T,
-                    logit_tau_T2,
-                    logit_tau_DT,
-                    logit_tau_D2)
-posterior_dists = hmm.posterior_marginals(observations)
-posterior_probs = posterior_dists.probs_parameter().numpy()
-
-def plot_state_posterior(ax, state_posterior_probs, title):
-  ln1 = ax.plot(state_posterior_probs, c='blue', lw=3, label='p(state | observations)')
-  ax.set_ylim(0., 1.1)
-  ax.set_ylabel('posterior probability')
-  ax2 = ax.twinx()
-  ln2 = ax2.plot(observations, c='black', alpha=0.3, label='observed observations')
-  ax2.set_title(title)
-  ax2.set_xlabel("time")
-  lns = ln1+ln2
-  labs = [l.get_label() for l in lns]
-  ax.legend(lns, labs, loc=4)
-  ax.grid(True, color='white')
-  ax2.grid(False)
-
-# PLOT MARGINAL POSTERIORS PROBABILITIES OF EACH STATE
-fig = plt.figure(figsize=(10, 10))
-plot_state_posterior(fig.add_subplot(2, 2, 1),
-                     posterior_probs[:, 0],
-                     title="state 0 (rate {:.2f})".format(true_means[0, 0]))
-plot_state_posterior(fig.add_subplot(2, 2, 2),
-                     posterior_probs[:, 1],
-                     title="state 1 (rate {:.2f})".format(true_means[1, 0]))
-plot_state_posterior(fig.add_subplot(2, 2, 3),
-                     posterior_probs[:, 2],
-                     title="state 2 (rate {:.2f})".format(true_means[2, 0]))
-plot_state_posterior(fig.add_subplot(2, 2, 4),
-                     posterior_probs[:, 3],
-                     title="state 3 (rate {:.2f})".format(true_means[3, 0]))
-plt.tight_layout()
 
 plt.figure()
 plt.plot(loss_history)
